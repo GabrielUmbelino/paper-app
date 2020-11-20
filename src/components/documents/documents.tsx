b5a05d9646259428097c686b145a3b905c17b20f
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2020-11-20 12:32:13 +0100

    create documents component

---
+++ src/components/documents/documents.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  CssBaseline,
+  Container,
+} from "@material-ui/core";
+
+export const Documents = () => {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">Documents</Container>
+    </React.Fragment>
+  );
+};
