b876187dedcd4fa7659baa816be67ae98fda4794
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2020-11-22 18:45:08 +0100

    refector

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import App from './app';
-import { render } from "@testing-library/react";
+import { render } from '@testing-library/react';
 
 describe('app', () => {
   it('renders learn react link', () => {
     const { container } = render(<App />);
     expect(container).toBeInTheDocument();
   });
-})
+});
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,11 @@
 import './app.css';
 import React from 'react';
-import { Header } from '../header/header';
 import { Routes } from '../../routes';
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Header />
         <Routes />
       </React.Fragment>
     );
