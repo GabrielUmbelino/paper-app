c31eadf28a5ceaa54c092f3de0a3acf9360b08dc
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2020-11-25 11:11:04 +0100

    add froala editor

---
+++ src/components/editor/froala-editor.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Require Editor CSS files.
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+
+import FroalaEditorComponent from 'react-froala-wysiwyg';
+
+export const FroalaEditor = () => <FroalaEditorComponent tag="textarea" />;
